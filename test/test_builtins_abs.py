3f6913edc1aea5241f3c039f7a1a7d0b170e8527
MC <taggedzi.mpc@gmail.com>
2018-03-04 09:39:05 -0500

    Modified config.yml to execute python unittest

--- test/test_builtins_abs.py
+++ test/test_builtins_abs.py
@@ -1,5 +1,5 @@
-import unittest
-from unittest import mock
+import unittest.mock
+from unittest.mock import patch, call
 from mock import call, patch
 from attributes.builtins.abs import print_abs, abs_integer, abs_float, abs_complex, abs_string
 
