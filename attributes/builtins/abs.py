367511934eb1d40e9b9b3dd84386e2d21b55fc28
MC <taggedzi.mpc@gmail.com>
2018-03-04 02:00:07 -0500

    Added Test class for abs created random_gen functions modified requirements to require mock

--- attributes/builtins/abs.py
+++ attributes/builtins/abs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import random
+from lp_utilities.random_gen import random_signed_integer, random_signed_float, random_signed_complex
 from lp_utilities.separator import separator
 
 """
@@ -12,29 +12,64 @@ For more information about complex numbers in python see:
 * Complex - http://python-reference.readthedocs.io/en/latest/docs/functions/complex.html
 """
 
+NUMBER_OF_TESTS_TO_RUN = 6
+
 
 def print_abs(input_value, output_value):
+    """
+    This method just does formatting for the abs function examples
+    :param input_value:   Number  The value sent to the abs() function
+    :param output_value:  Number  The value the abs() function returns
+    :return: Void
+    """
     print("abs({}) = {}".format(input_value, output_value))
 
 
-separator("abs(integer) - Absolute Value - Integer")
-for integer_ in range(-3, 4):
-    result = abs(integer_)
-    print_abs(integer_, result)
+def abs_integer():
+    separator("abs(integer) - Absolute Value - Integer")
+    for _ in range(NUMBER_OF_TESTS_TO_RUN):
+        integer_ = random_signed_integer(100)
+        result = abs(integer_)
+        print_abs(integer_, result)
 
 
-separator("abs(float) - Absolute Value - Floating Point")
-for _ in range(6):
-    # Generate a random float with a random sign
-    float_ = random.random() * 5 * random.randint(-100, 100)
-    result = abs(float_)
-    print_abs(float_, result)
+def abs_float():
+    separator("abs(float) - Absolute Value - Floating Point")
+    for _ in range(NUMBER_OF_TESTS_TO_RUN):
+        # Generate a random float with a random sign
+        float_ = random_signed_float(100)
+        result = abs(float_)
+        print_abs(float_, result)
 
 
-separator("abs(complex) - Absolute Value - Complex Numbers - Returns the absolute value of the magnitude of the "
-          "complex number.")
-for _ in range(6):
-    # Generate a random complex number with a random sign
-    complex_ = complex(0, random.random() * 5 * random.randint(-100, 100))
-    result = abs(complex_)
-    print_abs(complex_, result)
+def abs_complex():
+    separator("abs(complex) - Absolute Value - Complex Numbers - Returns the absolute value of the magnitude of the "
+              "complex number.")
+    for _ in range(NUMBER_OF_TESTS_TO_RUN):
+        # Generate a random complex number with a random sign
+        complex_ = random_signed_complex(100)
+        result = abs(complex_)
+        print_abs(complex_, result)
+
+
+def abs_string():
+    separator("abs(string) - Absolute Value - String")
+    try:
+        print_abs("5", abs("5"))
+    except TypeError as e:
+        print("Attempted to execute abs(\"{}\") and it throws a TypeError.".format(5))
+        print("Strings cannot be passed to the abs function even if the contents of the string are numbers.")
+        print("The exception message is:")
+        print(e)
+
+
+def main():
+    abs_integer()
+    abs_float()
+    abs_complex()
+    abs_string()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
--- attributes/builtins/all.py
+++ attributes/builtins/all.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from lp_utilities.separator import separator
 
+
 """
 The "all()" function simply tests to see if ALL of the elements in an iterable evaluate to a True condition
 
@@ -17,15 +18,27 @@ def print_all(input_, result_):
     print("Running all({}) returns {}".format(input_, result_))
 
 
-# Demo a list of values that evaluate to true
-separator("all(iterable) - with all items iterable - Returns true if all elements are iterable (non empty)")
-good_list = [1, 2, 3, 4, 5, 'Cat', {'Carbon'}, ["Bacon"]]
-result = all(good_list)
-print_all(good_list, result)
+def all_list_with_all_good_elements():
+    # Demo a list of values that evaluate to true
+    separator("all(iterable) - with all items iterable - Returns true if all elements are iterable (non empty)")
+    good_list = [1, 2, 3, 4, 5, 'Cat', {'Carbon'}, ["Bacon"]]
+    result = all(good_list)
+    print_all(good_list, result)
 
 
-# Demo a list with 1 value that evaluates to false
-separator("all(iterable) - with non-iterable items - Returns false if any elements are non iterable (empty)")
-bad_list = [1, 2, None, 4, 5]
-result = all(bad_list)
-print_all(bad_list, result)
+def all_list_with_empty_element():
+    # Demo a list with 1 value that evaluates to false
+    separator("all(iterable) - with non-iterable items - Returns false if any elements are non iterable (empty)")
+    bad_list = [1, 2, None, 4, 5]
+    result = all(bad_list)
+    print_all(bad_list, result)
+
+
+def main():
+    all_list_with_all_good_elements()
+    all_list_with_empty_element()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
